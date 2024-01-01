1eada806afed82f070fe375cec75a4f87b611661
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-01 18:52:07 -0300

    ✨ Add: Create views functions for cart and checkout pages, views.py

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render, HttpResponse
 
 def index(request):
-    return render(request, "src/index.html")
+    context = {}
+    return render(request, "src/index.html", context)
+
+def cart(request):
+    context = {}
+    return render(request, "src/cart.html", context)
+
+def checkout(request):
+    context = {}
+    return render(request, "src/checkout.html", context)
