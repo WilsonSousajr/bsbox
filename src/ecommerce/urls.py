0e1da6f5e8b82ae239ee66fd06814e63fd7460fb
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-01 18:51:39 -0300

    ✨ Add:  add paths to cart/, checkout/ in urlpatterns, urls.py

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-app_name = "eccomerce"
+app_name = "ecommerce"
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("cart/", views.cart, name="cart"),
+    path("checkout/", views.checkout, name="checkout"),
 ]
