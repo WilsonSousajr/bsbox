807a5612ec48ad672e7c042d0d8b0829a4f4ae46
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-04 14:28:07 -0300

    ✨ Add: Create cart items logic, views.py

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -4,6 +4,16 @@ from django.http import JsonResponse
 import json
 
 def index(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
     products = Product.objects.all()
     context = {'products': products}
     return render(request, "src/index.html", context)
