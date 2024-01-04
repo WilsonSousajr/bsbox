031654b10e2bd9d5721fdfcc1fe8c061bdd8f885
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-04 14:31:41 -0300

    ✨ Add: Create updateItem view, views.py

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -43,3 +43,28 @@ def checkout(request):
         cartItems = order['get_cart_items']
     context = {'items':items, "order":order, 'cartItems': cartItems}
     return render(request, "src/checkout.html", context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print(f'Action: {action}')
+    print(f'Product: {productId}')
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+    
+    return JsonResponse('Item was added', safe=False)
