99010dc9ee57ee5aa6db46f2d6d39550b4f5f0be
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-04 14:29:25 -0300

    ✨ Add: Add cartItems variable to cart view, views.py

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -23,6 +23,7 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
