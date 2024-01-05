71131801abb8528dced98aae7b40d3a2cfdaf8ff
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-05 16:48:48 -0300

    🎨✨ Edit: Use cartData() to declare checkout view variables

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -24,15 +24,12 @@ def cart(request):
     return render(request, "src/cart.html", context)
 
 def checkout(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-        cartItems = order['get_cart_items']
+    data = cartData(request)
+
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
     context = {'items':items, "order":order, 'cartItems': cartItems}
     return render(request, "src/checkout.html", context)
 
