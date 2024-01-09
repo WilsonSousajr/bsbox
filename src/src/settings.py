c9220b977ab0ead4e979c4aed44028e5b309f10c
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-09 14:27:30 -0300

    Prepare deploy stuff

--- src/src/settings.py
+++ src/src/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = 'images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
