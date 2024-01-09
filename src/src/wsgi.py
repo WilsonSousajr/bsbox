ec86b0f2bd89631c991528b6293933dcdbd29a8f
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-08 21:39:24 -0300

    Prepare to deploy

--- src/src/wsgi.py
+++ src/src/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.src.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bsbox.settings")
 application = get_wsgi_application()
 app = application
