074ff1d4e2a2ec71122acd7a3a83ca13000b6e3e
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-09 13:30:17 -0300

    Prepare to deploy application

--- src/src/wsgi.py
+++ src/src/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
