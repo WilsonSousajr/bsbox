6de79ed14d2955b90b26dad743caa246b2488cb5
Wilson Sousa <willnapolitano21@gmail.com>
2024-01-08 21:49:06 -0300

    Prepare to deploy

--- src/src/wsgi.py
+++ src/src/wsgi.py
@@ -8,8 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bsbox.settings")
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
+application = Cling(get_wsgi_application())
 app = application
